02522f24f63713e950b15c829a5e66242d8495cf
JamaHCS <jama117@hotmail.com>
2019-08-09 15:43:04 -0500

    mitad de clase de strings

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hola");
+var nombre = 'Jama'
+var apellido = 'Ideal'
+var edad = 19
+var peso = '65'
+var nombreMayus = nombre + ' ' + apellido
+var nombreMayusculas = nombreMayus.toUpperCase()
+
+console.log
+(
+    'Hola, soy: ' + nombre + ' ' + apellido
+)
+
+console.log("Tengo " + edad + " años")
+
+console.log('Hola, soy: ' + nombreMayusculas)
