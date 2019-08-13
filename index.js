ecee831093120489ae8cb60a24586747b22ae16d
JamaHCS <jama117@hotmail.com>
2019-08-13 16:14:12 -0500

    Funcion para la mayoria de edad, apenas iniciando clase

--- index.js
+++ index.js
@@ -1,9 +1,55 @@
-var x = 4, y = '4'
-
 var jama = {
-    nombre: 'Jama'
+    nombre: 'Héctor',
+    apellido: 'Escobedo',
+    edad: 19,
+    nickname: 'Jama Ideal',
+    ingeniero: false,
+    frontendDeveloper: true,
+    taekwondoin: true,
+    elAmordeTuVida: true,
+    cocinero: false,
+    guitarrista: false,
+    dj: false,
+    }
+
+    function imprimirFunciones (persona)
+    {
+        console.log(`${persona.nickname} es:`)
+        if(persona.ingeniero)
+        {
+            console.log('ingeniero')
+        }
+        if (persona.cocinero)
+        {
+            console.log('cocinero')
+        }
+        if (persona.dj)
+        {
+            console.log('dj')
+        }
+        if (persona.elAmordeTuVida)
+        {
+            console.log('elAmorDeTuVida')
+        }
+        if (persona.frontendDeveloper)
+        {
+            console.log('frontendDeveloper')
+        }
+        if (persona.taekwondoin)
+        {
+            console.log('taekwondoin')
+        }
+        if (persona.guitarrista)
+        {
+            console.log('guitarrista')
+        }
+    }
+
+function imprimirMayorEdad (persona)
+{
+    
 }
 
-var apolo = {
-    nombre: 'Jama'
-}
+
+imprimirFunciones(jama)
+
