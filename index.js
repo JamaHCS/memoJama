60dcac4a72cd45a0eef7d80cd2ca2f3f583c4836
JamaHCS <jama117@hotmail.com>
2019-08-13 14:45:27 -0500

    el == no es igual a ===, el segundo es mas estrcito en la estructura para la comparacion de las variables

--- index.js
+++ index.js
@@ -1,32 +1,9 @@
+var x = 4, y = '4'
+
 var jama = {
-    nombre: 'Jama',
-    apellido: 'Ideal',
-    edad: 19,
-    nickname: 'Hector',
+    nombre: 'Jama'
 }
 
-function imprimirNombreUpper ({nombre})
-{
-    console.log(`El nombre es: ${nombre.toUpperCase()}`)
-}
-
-//imprimirNombreUpper (jama)
-
-function printNameAndAge (persona)
-{
-    console.log
-    (
-        `Hola, me llamo ${persona.nombre} y tengo ${persona.edad} años`
-    )
-}
-
-printNameAndAge (jama)
-
-function cumpleaños (persona)
-{
-    return {
-        ...persona,
-        edad: persona.edad + 1
-    }
-
+var apolo = {
+    nombre: 'Jama'
 }
