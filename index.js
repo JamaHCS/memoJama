8e2261abcf7a9b74ba6ae4b6d1cfe65772cb414b
JamaHCS <jama117@hotmail.com>
2019-08-14 14:36:49 -0500

    stahs

--- index.js
+++ index.js
@@ -1,14 +1,49 @@
-var contador = 0
-
-const llueve = () => Math.random() < 0.25
-
-do{
-    contador++
+var hector = {
+    nombre: 'Héctor',
+    apellido: 'Escobedo',
+    edad: 19,
 }
-while(!llueve())
-    if(contador === 1){
-        console.log(`Fui a ver si llovia ${contador} vez`)
-    }
-    else{
-        console.log(`Fui a ver si llovia ${contador} veces`)
-    }
+
+var jama = {
+    nombre: 'Jama',
+    apellido: 'Ideal',
+    edad: 19,
+}
+
+var dulce = {
+    nombre: 'Dulce',
+    apellido: 'Reyes',
+    edad: 16,
+}
+
+var ninja = {
+    nombre: 'Misael',
+    apellido: 'Tapia',
+    edad: 22,
+}
+
+var psicosis = {
+    nombre: 'Aldito',
+    apellido: 'García',
+    edad: 23,
+}
+
+var lalo = {
+    nombre: 'Eduardo',
+    apellido: 'Hernandez',
+    edad: 21,
+}
+
+var personas = [
+    hector,
+    jama,
+    dulce,
+    ninja,
+    psicosis,
+    lalo
+]
+
+for(var i = 0; i < personas.length; i++){
+    var persona = personas[i]
+    console.log(`${persona.nombre} tiene ${persona.edad} años`)
+}
