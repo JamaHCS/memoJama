5797e468a7e19f47cc882247848403cb841278b9
JamaHCS <jama117@hotmail.com>
2019-08-15 14:14:50 -0500

    prototipos y clases

--- index.js
+++ index.js
@@ -1,72 +1,24 @@
-var hector = {
-    nombre: 'Héctor',
-    apellido: 'Escobedo',
-    edad: 19,
-    libros: 45,
-    estatura:1.8
-}
+class Persona {
+    constructor(nombre, apellido, altura) {
+        this.nombres = nombre
+        this.apellidos = apellido
+        this.alturas = altura
+    }
+    saludar(){
+        console.log(`Hola, me llamo ${this.nombres}`)
+    }
+    soyAlto(){
+        return this.alturas >= 1.8
 
-var jama = {
-    nombre: 'Jama',
-    apellido: 'Ideal',
-    edad: 35,
-    estatura: 1.78
-}
-
-var dulce = {
-    nombre: 'Dulce',
-    apellido: 'Reyes',
-    edad: 16,
-    libros: 91,
-    estatura: 1.60
-}
-
-var ninja = {
-    nombre: 'Misael',
-    apellido: 'Tapia',
-    edad: 22,
-    libros: 15,
-    estatura: 1.82
-}
-
-var psicosis = {
-    nombre: 'Aldito',
-    apellido: 'García',
-    edad: 23,
-    libros: 42,
-    estatura: 1.60
-}
-
-var lalo = {
-    nombre: 'Eduardo',
-    apellido: 'Hernandez',
-    edad: 21,
-    estatura: 1.70,
-    libros:74,
-}
-const esAlta = ({estatura}) => estatura > 1.70
-
-var personas = [
-    hector,
-    jama,
-    dulce,
-    ninja,
-    psicosis,
-    lalo
-]
-
-var personasAltas = personas.filter(esAlta)
-
-const pasarAltura = persona => {
-    return {
-        ...persona,
-        estatura: persona.estatura * 100
     }
 }
-
-var personasCms = personas.map(pasarAltura)
-
-
-
-// console.log(personasCms)
-
+class Desarrollador extends Persona {
+    constructor(nombre, apellido, altura) {
+        super(nombre, apellido, altura)
+    }
+    saludar(){
+        console.log(`Hola, me llamo ${this.nombres} y soy desarrollador`)
+    }
+}
+// var jama = new Desarrollador('Jama', 'Ideal', 1.8)
+// var dulce = new Persona('Dulce', 'Reyes', 1.60)
