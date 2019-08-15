af692c7ff476fd5101c043d20cbbb64fb0b627a9
JamaHCS <jama117@hotmail.com>
2019-08-15 14:30:32 -0500

    contestando el saludo

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ class Persona {
         this.apellidos = apellido
         this.alturas = altura
     }
-    saludar(){
+    saludar(fn){
         console.log(`Hola, me llamo ${this.nombres}`)
+        if(fn){
+            fn(this.nombres)
+        }
     }
     soyAlto(){
         return this.alturas >= 1.8
@@ -16,9 +19,25 @@ class Desarrollador extends Persona {
     constructor(nombre, apellido, altura) {
         super(nombre, apellido, altura)
     }
-    saludar(){
+    saludar(fn){
         console.log(`Hola, me llamo ${this.nombres} y soy desarrollador`)
+        if (fn) {
+            fn(this.nombres, true)
+        }
     }
 }
-// var jama = new Desarrollador('Jama', 'Ideal', 1.8)
-// var dulce = new Persona('Dulce', 'Reyes', 1.60)
+
+function responderSaludo (nombre, esDev) {
+    console.log(`Buen dia, ${nombre}`)
+    if(esDev){
+        console.log(`ah, mira, no sabía que eras dev`)
+    }
+}
+
+
+var jama = new Desarrollador('Jama', 'Ideal', 1.8)
+var dulce = new Persona('Dulce', 'Reyes', 1.60)
+
+
+jama.saludar(responderSaludo)
+dulce.saludar(responderSaludo)
