492350f09bf9b645e1774fcd6ab58e6febfb368e
JamaHCS <jama117@hotmail.com>
2019-08-15 15:16:29 -0500

    asincronismo

--- index.js
+++ index.js
@@ -1,43 +1,11 @@
-class Persona {
-    constructor(nombre, apellido, altura) {
-        this.nombres = nombre
-        this.apellidos = apellido
-        this.alturas = altura
-    }
-    saludar(fn){
-        console.log(`Hola, me llamo ${this.nombres}`)
-        if(fn){
-            fn(this.nombres)
-        }
-    }
-    soyAlto(){
-        return this.alturas >= 1.8
+setTimeout(function(){
+    console.log(`fin`)
+},1000)
 
-    }
-}
-class Desarrollador extends Persona {
-    constructor(nombre, apellido, altura) {
-        super(nombre, apellido, altura)
-    }
-    saludar(fn){
-        console.log(`Hola, me llamo ${this.nombres} y soy desarrollador`)
-        if (fn) {
-            fn(this.nombres, true)
-        }
+function hola(){
+    for(var i = 0; i< 100000 ; i++){
+    console.log(i)
     }
 }
 
-function responderSaludo (nombre, esDev) {
-    console.log(`Buen dia, ${nombre}`)
-    if(esDev){
-        console.log(`ah, mira, no sabía que eras dev`)
-    }
-}
-
-
-var jama = new Desarrollador('Jama', 'Ideal', 1.8)
-var dulce = new Persona('Dulce', 'Reyes', 1.60)
-
-
-jama.saludar(responderSaludo)
-dulce.saludar(responderSaludo)
+hola()
