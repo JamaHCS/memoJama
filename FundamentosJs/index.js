95b3650034d809e5ac70d0a824d805d284e676a6
JamaHCS <jama117@hotmail.com>
2019-08-16 15:41:36 -0500

    Promises

--- FundamentosJs/index.js
+++ FundamentosJs/index.js
@@ -1,13 +1,16 @@
-const apiUrl = 'https://swapi.co/api/'
+const URL = 'https://swapi.co/api'
+const PEOPLE_URL = '/people/:id'
 
-const peopleUrl = 'people/:id'
-const lukeUrl = `${apiUrl}${peopleUrl.replace(':id', 1)}`
-const op = { crossDomain: true }
-
-function obtenerPersonaje(id){
-    const
+const getCharacter = (id) => new Promise((resolve, rejected) => {
+    fetch(`${URL}${PEOPLE_URL.replace(':id', id)}`)
+        .then((response) => resolve(response.json()))
+        .catch(() => rejected(id))
+})
+const onError = (id) => {
+    console.log(`Hubo un error con el personaje ${id}`)
 }
-
-$.get(lukeUrl,op, function(luke){
-    console.log(`Hola, yo soy, ${luke.name}`)
+getCharacter(1).then((character) => {
+    console.log(`Hola, yo soy ${character.name}`)
+}).catch((id) => {
+    onError(id)
 })
